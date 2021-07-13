fe68899230e24f5b9d4a354d20d7b01c21cfcb87
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-13 09:42:42 +0200

    Add project files.

---
+++ Es1/Es1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Es1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 18;
+            //age = null; dà errore
+
+            bool y = true; //default false
+            string s1 = "Hello";
+            string s2 = null;
+            string s3 = "";
+
+            object obj1 = new object();
+            object obj2 = null;
+            object obj3 = obj1;
+            //obj3 punta allo stesso spazio di obj1
+
+            int numero = 20;
+            var numero2 = 30;
+            var s = "Ciao";
+            var s1 = null;
+
+        }
+    }
+}
