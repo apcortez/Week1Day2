7a8bead25d413e28f8b8cdd253fbe5f6d0292f4e
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-13 10:42:13 +0200

    conversione di stringa in un intero

--- Es3/Es3/Program.cs
+++ Es3/Es3/Program.cs
@@ -15,6 +15,11 @@ namespace Es3
             //c = d; dà errore! c'è bisogno di un casting esplicito --> da un contenitore grande a un contenitore più piccolo --> potenziale perdita di informazione
             c = (int)d;
             Console.WriteLine(c);
+
+            string s = "123";
+            //int n = (int)s; non si può fare il casting diretto tra un intero e una string e viceversa. C'è bisogno di un converter.
+            int n = int.Parse(s);
+            Console.WriteLine(n);
         }
     }
 }
