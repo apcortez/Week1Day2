e0a622ec09648f58a081fc50b5a5933eae741afb
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-13 10:37:55 +0200

    casting implicito ed esplicito

---
+++ Es3/Es3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Es3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Casting
+            int a = 2;
+            long b = a; //cast implicito
+
+            int c;
+            long d = 9999999999;
+            //c = d; dà errore! c'è bisogno di un casting esplicito --> da un contenitore grande a un contenitore più piccolo --> potenziale perdita di informazione
+            c = (int)d;
+            Console.WriteLine(c);
+        }
+    }
+}
