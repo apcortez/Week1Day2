a18d2f137110b1b94aa948c05a17f219fee8b927
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-13 12:31:49 +0200

    ciclo For

---
+++ Es4/Es4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Es4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Valore: {i}");
+            }
+
+        }
+    }
+}
