a76ab9eaf5c9fd22c19781a70a7aac4f43f155ef
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-13 09:49:12 +0200

    modi di assegnazione boolean

---
+++ Es2/Es2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Es2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x, y = true; //x = false in questo caso
+            x = true;
+            x = !y; //nega valore, x torna a false
+            x = (18 > 9);
+
+        }
+    }
+}
